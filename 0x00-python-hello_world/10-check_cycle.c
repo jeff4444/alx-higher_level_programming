5940d30c87d11c2550836a7ce9ac0d947b2a569e
Jeff Mofor <allojeff444@gmail.com>
2023-06-06 23:46:42 -0700

    Advanced tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is aa cycle in a linked list
+ * @list: linked list
+ * Return: 1 (True) 0 (False)
+ */
+int check_cycle(listint_t *list)
+{
+
+}
