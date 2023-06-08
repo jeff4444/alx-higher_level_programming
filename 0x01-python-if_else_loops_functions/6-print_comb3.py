b68036426da84e4d0eb18ee554715c52640b484c
Jeff Mofor <allojeff444@gmail.com>
2023-06-07 18:50:45 -0700

    Fixed the sixth

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,5 @@ for i in range(1, 100):
     else:
         if i == 89:
             print("{}".format(i))
-        if (i - ((i // 10) * 10)) not in range((i // 10) + 1):
+        elif (i - ((i // 10) * 10)) not in range((i // 10) + 1):
             print("{:02d}".format(i), end=", ")
