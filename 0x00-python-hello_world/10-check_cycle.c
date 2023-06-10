f96335223ac5d84dba6e1670ef4e31a1ffaf4333
Jeff Mofor <allojeff444@gmail.com>
2023-06-10 13:12:22 -0700

    Still working on tech interview 0

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,24 +9,19 @@ int check_cycle(listint_t *list)
 {
 	listint_t *current;
 	listint_t *check;
-	int i, j;
 
-	i = 0;
 	current = list;
 	while (current != NULL)
 	{
-		check = list;
-		j = 0;
-		while (j != i)
+		check = current->next;
+		while (check != NULL)
 		{
 			if (current == check)
 				return (1);
 			else
 				check = check->next;
-			j++;
 		}
 		current = current->next;
-		i++;
 	}
 	return (0);
 }
