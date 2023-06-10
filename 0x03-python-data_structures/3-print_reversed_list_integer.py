c4fe1db0c0c8bd799efde7c26f74ae46d715881a
Jeff Mofor <allojeff444@gmail.com>
2023-06-10 08:25:59 -0700

    Added new line for empty matrix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[len(my_list) - 1 - i]))
+    if len(my_list) == 0:
+        print()
+    else:
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[len(my_list) - 1 - i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for elem in row:
-            if elem == row[len(row) - 1]:
-                print("{:d}".format(elem))
-            else:
-                print("{:d}".format(elem), end=" ")
+    if len(matrix) == 0:
+        print()
+    else:
+        for row in matrix:
+            for elem in row:
+                if elem == row[len(row) - 1]:
+                    print("{:d}".format(elem))
+                else:
+                    print("{:d}".format(elem), end=" ")
