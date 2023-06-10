300bd9ea395abdebe3d5cc723b0d2cfda324f116
Jeff Mofor <allojeff444@gmail.com>
2023-06-10 13:50:57 -0700

    reset

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,19 +9,24 @@ int check_cycle(listint_t *list)
 {
 	listint_t *current;
 	listint_t *check;
+	int i, j;
 
+	i = 0;
 	current = list;
 	while (current != NULL)
 	{
-		check = current->next;
-		while (check != NULL)
+		check = list;
+		j = 0;
+		while (j != i)
 		{
 			if (current == check)
 				return (1);
 			else
 				check = check->next;
+			j++;
 		}
 		current = current->next;
+		i++;
 	}
 	return (0);
 }
