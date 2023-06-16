00114fb20a02b0453aee3d6eabb042f946a87be0
Jeff Mofor <allojeff444@gmail.com>
2023-06-16 08:47:59 -0700

    User can specify path

--- sortfiles/sort.py
+++ sortfiles/sort.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 import os
 import shutil
+import sys
 
-files = os.listdir()
+if (len(sys.argv) < 2):
+    maindir = '.'
+else:
+    if os.path.isdir(sys.argv[1]):
+        maindir = sys.argv[1]
+    else:
+        print("Your entered a non-valid path")
+        sys.exit(1)
+print(len(sys.argv))
+files = os.listdir(maindir)
 
 extensions = set()
 for some_file in files:
-    if os.path.isfile(some_file):
+    if os.path.isfile(maindir + '/' + some_file):
         _, ext = os.path.splitext(some_file)
         extensions.add(ext)
 
@@ -15,9 +25,11 @@ for ext in extensions:
         dirname = 'none'
     else:
         dirname = ext[1:]
-    if os.path.isdir("./" + dirname) is False:
-        os.mkdir(dirname)
+    if os.path.isdir(maindir + "/" + dirname) is False:
+        os.mkdir(maindir + '/' + dirname)
     for some_file in files:
+        if os.path.isdir(maindir + '/' + some_file):
+            continue
         _, file_ext = os.path.splitext(some_file)
         if file_ext == ext:
-            shutil.move(some_file, dirname)
+            shutil.move((maindir + '/' + some_file), (maindir + '/' + dirname))
