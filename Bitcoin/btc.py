e79dfdc2dd56900ba26a0d563b6f4e9771df8c85
Jeff Mofor <allojeff444@gmail.com>
2023-06-18 22:17:35 -0700

    Fixed some bugs

--- Bitcoin/btc.py
+++ Bitcoin/btc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+from main import main
 from forex_python.bitcoin import BtcConverter
 
 def btc_exchange():
--- Bitcoin/forex.py
+++ Bitcoin/forex.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-from main import main
 from forex_python.converter import CurrencyRates
 import datetime
 
@@ -26,7 +25,7 @@ def print_rates(dictionary, c):
 
 def latest_currencies():
     rate = CurrencyRates()
-    currency = input("Enter currency: ").strip()
+    currency = input_currency("Enter currency: ")
     option = int(get_valid_response("Select option:\n[1] View Rates as of now\n[2] View Rates at a user specified date and time: ", "12"))
 
     if option == 2:
@@ -50,15 +49,15 @@ def latest_currencies():
 def print_conversion(base, dest, amt, res):
     print(f'{amt} {base} = {res} {dest}')
 
+def print_pair_rate(base, dest, res):
+    print(f'1 {base} = {res} {dest}')
+
 
 def convert_amount():
     rate = CurrencyRates()
-    base_cur = input("Enter base currency: ")
-    dest_cur = input("Enter destination currency: ")
-    try:
-        amount = float(input("Amount: "))
-    except:
-        print("Not a valid number")
+    base_cur = input_currency("Enter base currency: ")
+    dest_cur = input_currency("Enter destination currency: ")
+    amount = input_float("Enter amount: ")
     option = int(get_valid_response("Select option:\n[1] View coversion as of now\n[2] View conversion at a user specified date and time: ", "12"))
     if option == 2:
         y, m, d, h, mi, s, ms = get_valid_date()
@@ -81,7 +80,28 @@ def convert_amount():
 
 
 def two_pairs_exchange():
-    pass
+    rate = CurrencyRates()
+    base_cur = input_currency("Enter base currency: ")
+    dest_cur = input_currency("Enter destination currency: ")
+    option = int(get_valid_response("Select option:\n[1] View Rates as of now\n[2] View Rates at a user specified date and time: ", "12"))
+    if option == 2:
+        y, m, d, h, mi, s, ms = get_valid_date()
+        date_obj = datetime.datetime(y, m, d, h, mi, s, ms)
+        try:
+            print_pair_rate(base_cur, dest_cur, rate.get_rate(base_cur, dest_cur, date_obj))
+        except:
+            print("An error occured")
+    else:
+        try:
+            print_pair_rate(base_cur, dest_cur, rate.get_rate(base_cur, dest_cur))
+        except:
+            print("An error occured")
+    option = int(get_valid_response("Select Option:\n[1] Return To main\n[2] Check exchange rate again\n[3] Quit\nEnter response: ", "123"))
+    if option == 1:
+        main()
+    elif option == 2:
+        two_pairs_exchange()
+
 
 def fiat_exchange():
     print("Select Option: \n \
@@ -96,3 +116,23 @@ def fiat_exchange():
         two_pairs_exchange()
     else:
         convert_amount()
+
+def input_currency(text):
+    code = CurrencyRates()
+    cur = input(text).strip().upper()
+    try:
+        c = code.get_rates(cur)
+        return cur
+    except:
+        print(f'{cur} not found. Try again')
+        cur = input_currency(text)
+        return cur
+
+def input_float(text):
+    try:
+        amount = float(input(text).strip())
+        return amount
+    except:
+        print("The amount you entered isn't valid! Try again")
+        amount = input_float(text)
+        return amount
