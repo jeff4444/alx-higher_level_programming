5e974e3c342ef4ee0d73dd651e893201ded9fea8
Jeff Mofor <allojeff444@gmail.com>
2023-06-28 18:32:25 -0700

    Initial commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+def safe_print_list_integers(my_list=[], x=0):
+    i, j = 0, 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            i += 1
+            j += 1
+        except:
+            i += 1
+    return j
