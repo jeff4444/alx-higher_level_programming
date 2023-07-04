09a4c8b1d564545828ffecda20a01e4c025ef729
Jeff Mofor <allojeff444@gmail.com>
2023-07-03 18:30:01 -0700

    Added repr module

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -62,4 +62,5 @@ class Rectangle:
 
     def __repr__(self):
         """Returns Rectangle"""
-        return 'Rectangle(' + str(self.__width) + ', ' + str(self.__height) + ')'
+        return ('Rectangle(' + str(self.__width) + ', ' +
+                str(self.__height) + ')')
