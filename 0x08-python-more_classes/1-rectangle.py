7c4a0b81e756f5a007b0d3c467e9e78a57085c84
Jeff Mofor <allojeff444@gmail.com>
2023-07-03 18:05:29 -0700

    More bugs fixed

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -35,5 +35,13 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle"""
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
         self.__width = width
+        if not isinstance(height, int):
+            raise TypeError('width must be an integer')
+        if height < 0:
+            raise ValueError('width must be >= 0')
         self.__height = height
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -35,7 +35,15 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle"""
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
         self.__width = width
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
         self.__height = height
 
     def area(self):
