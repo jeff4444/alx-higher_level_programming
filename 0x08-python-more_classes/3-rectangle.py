6edd39d6cccac08872e0a20a0792a7e53686352c
Jeff Mofor <allojeff444@gmail.com>
2023-07-03 18:18:33 -0700

    More bugs fixed

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
         """Set width"""
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        if width < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -29,7 +29,7 @@ class Rectangle:
         """Set height"""
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        if height < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
 
