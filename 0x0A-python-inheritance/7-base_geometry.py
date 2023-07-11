73d664286c55dc6e500d2001f0c840a99114d720
Jeff Mofor <allojeff444@gmail.com>
2023-07-11 12:07:03 -0700

    Defined init method

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,11 +5,17 @@
 class BaseGeometry:
     """BaseGeometry class"""
 
+    def __init__(self):
+        """Init function"""
+        pass
+
     def area(self):
+        """Area function"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
+        """int validator"""
+        if type(value) != int:
             raise TypeError(f'{name} must be an integer')
         if value <= 0:
             raise ValueError(f'{name} must be greater than 0')
