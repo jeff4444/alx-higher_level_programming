e56d6cc322a348d4cb4b70995d3b5737cf160282
Jeff Mofor <allojeff444@gmail.com>
2023-07-11 16:40:36 -0700

    reload from json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return sthe json object of self"""
+        if attrs is None:
+            return self.__dict__
+        return dict(list(filter((lambda x: x[0] in attrs),
+                    self.__dict__.items())))
+
+    def reload_from_json(self, json):
+        """Reload object from json"""
+        for key, val in json.items():
+            self.key = val
