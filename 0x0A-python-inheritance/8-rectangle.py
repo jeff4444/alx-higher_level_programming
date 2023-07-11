dbd71a94de9c35a570ee47f06b9f4f9808ff64a2
Jeff Mofor <allojeff444@gmail.com>
2023-07-11 11:55:52 -0700

    Still testing

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# from '7-base_geometry' import BaseGeometry
 """Rectangle"""
 
 
+from '7-base_geometry' import BaseGeometry
 class Rectangle(BaseGeometry):
     """Rectangle"""
     def __init__(self, width, height):
