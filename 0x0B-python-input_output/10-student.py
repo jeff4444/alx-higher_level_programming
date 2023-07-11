37b993a99409bb6e9d1766be742f37f1a2ef582c
Jeff Mofor <allojeff444@gmail.com>
2023-07-11 16:25:24 -0700

    student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return sthe json object of self"""
+        if attrs is None:
+            return self.__dict__
+        return dict(list(filter((lambda x: return x[0] in attrs),
+                    self.__dict__.items())))
