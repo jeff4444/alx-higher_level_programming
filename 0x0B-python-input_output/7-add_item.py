c670e01503619910f584b0fed0fb31a3818a81af
Jeff Mofor <allojeff444@gmail.com>
2023-07-11 15:49:21 -0700

    Removed main

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,18 +6,13 @@ import json
 import sys
 
 
-def main():
-    """main function, will load, add and save object to json"""
-    filename = 'add_item.json'
-    with open(filename, 'r', encoding='utf-8') as f:
-        try:
-            cur_obj = json.load(f)
-        except json.decoder.JSONDecodeError:
-            cur_obj = []
-        for i in range(1, len(sys.argv)):
-            cur_obj.append(sys.argv[i])
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(cur_obj, f)
-
-
-main()
+filename = 'add_item.json'
+with open(filename, 'r', encoding='utf-8') as f:
+    try:
+        cur_obj = json.load(f)
+    except json.decoder.JSONDecodeError:
+        cur_obj = []
+    for i in range(1, len(sys.argv)):
+        cur_obj.append(sys.argv[i])
+with open(filename, 'w', encoding='utf-8') as f:
+    json.dump(cur_obj, f)
