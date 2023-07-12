b268262db38ec473a95aa0c8ed54bf7899705412
Jeff Mofor <allojeff444@gmail.com>
2023-07-11 19:41:46 -0700

    Lagging by one exponent solved

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
         return []
     triangle = []
     my_list = []
-    for i in range(1, n):
+    for i in range(1, n + 1):
         l, r = -1, 0
         prev_list = my_list
         my_list = []
