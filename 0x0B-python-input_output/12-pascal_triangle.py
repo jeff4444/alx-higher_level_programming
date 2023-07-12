ba2547fdf831d34b5f7a60e741c248011e3e9cf3
Jeff Mofor <allojeff444@gmail.com>
2023-07-11 19:33:34 -0700

    Solving pascal's traingle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,3 +6,24 @@ def pascal_triangle(n):
     """Pascal's triangle generator"""
     if n <= 0:
         return []
+    triangle = []
+    my_list = []
+    for i in range(1, n):
+        l, r = -1, 0
+        prev_list = my_list
+        my_list = []
+        if i == 1:
+            my_list = [1]
+            triangle.append(my_list)
+            continue
+        for j in range(i + 1):
+            if r == i:
+                my_list.append(prev_list[l])
+            elif l == -1:
+                my_list.append(prev_list[r])
+            else:
+                my_list.append(prev_list[l] + prev_list[r])
+            l += 1
+            r += 1
+        triangle.append(my_list)
+    return triangle
