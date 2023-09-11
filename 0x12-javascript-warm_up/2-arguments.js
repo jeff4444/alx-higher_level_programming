cfac5ba4aae888033b9226a11dbff626fdaa7c80
Jeff <allojeff444@gmail.com>
2023-09-11 08:44:43 -0700

    Testing...

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 const argc = process.argv.length;
-
-if (argc > 3)
-{
-	console.log("Arguments found");
-}
-else if (argc === 3)
-{
-	console.log("Argment found");
-}
-else
-{
-	console.log("No argument");
+if (argc > 3) {
+  console.log("Arguments found");
+} else if (argc === 3) {
+  console.log("Argment found");
+} else {
+  console.log("No argument");
 }
